bab84aa6328195c83b7da820d48aee7257daae4b
Daniel Mutwiri <mutwiri.daniel@yahoo.com>
2017-05-18 12:36:00 +0300

    Library Member Model - Using Delegation inheritance to copy features to another Model

--- models/models.py
+++ models/models.py
@@ -140,3 +140,13 @@ class BaseArchive(models.AbstractModel):
 	def do_archive(self):
 		for record in self:
 			record.active = not record.active
+
+
+class LibraryMember(models.Model):
+	_name = 'library.member'
+	_inherits = {'res.partner': 'partner_id'}
+	partner_id = fields.Many2one(
+		'res.partner', ondelete='cascade')
+	date_start = fields.Date('Member Since')
+	date_end = fields.Date('Termination Date')
+	member_number = fields.Char()
